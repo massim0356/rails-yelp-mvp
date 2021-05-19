83f44a8900ebdb9b61dd2d1ac60f8edb21f9507c
Massim Shah <massimshah@hotmail.com>
2021-05-19 11:45:43 +0900

    created associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates_associated :restaurant
+  validates :content, presence: true
+  validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
 end
